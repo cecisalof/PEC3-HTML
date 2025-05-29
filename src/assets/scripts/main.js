0b3c2ba9038bd392581fb4ef4907e38991c5839d
cecisalof <cecilia.sanchezlosada@gmail.com>
2025-05-29 12:01:33 +0200

    [Fix]: style fix

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -15,22 +15,22 @@ import agenda from "../../data/agenda.json";
 
 function renderTalk(talk) {
   return `
-    <div class="talk-card agenda-box bg-brand-cards col-span-full md:col-span-3 xl:col-span-4">
-      <p class="text-sm font-bold">${talk.time}</p>
-      <p class="text-base font-semibold my-2 text-white">${talk.topic}</p>
-      <p class="text-sm font-medium">${talk.speaker}</p>
-      <p class="text-xs mb-2 font-light">${talk.position}</p>
-      <p class="text-xs font-normal">${talk.description}</p>
-    </div>
+<div class="talk-card agenda-box bg-brand-cards col-span-full md:col-span-3 xl:col-span-4 font-heading">
+  <p class="text-sm md:text-base font-bold uppercase">${talk.time}</p>
+  <p class="text-base md:text-lg font-semibold my-2 text-white">${talk.topic}</p>
+  <p class="text-sm md:text-base font-semibold">${talk.speaker}</p>
+  <p class="text-xs md:text-sm mb-2 font-light">${talk.position}</p>
+  <p class="text-xs md:text-sm font-normal my-4">${talk.description}</p>
+</div>
   `;
 }
 
 function renderEvent(event) {
   return `
     <div class="agenda-box bg-brand-secondary col-span-full">
-      <p class="text-sm">${event.time} | ${event.title}</p>
-      ${event.topic ? `<p class="font-bold  text-white">${event.topic}</p>` : ""}
-      ${event.description ? `<p class="text-sm">${event.description}</p>` : ""}
+      <p class="text-sm md:text-base">${event.time} | ${event.title}</p>
+      ${event.topic ? `<p class="font-bold my-2  text-white text-base md:text-lg">${event.topic}</p>` : ""}
+      ${event.description ? `<p class="my-2 text-xs md:text-sm">${event.description}</p>` : ""}
     </div>
   `;
 }
